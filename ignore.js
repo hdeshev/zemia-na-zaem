b169f290b8964dc22af2bab3dcd06d286bb0c584
Hristo Deshev <hristo@deshev.com>
2011-05-12 18:54:10 +0300

    Minor code cleanup.

--- ignore.js
+++ ignore.js
@@ -1,18 +1,15 @@
 var ignored = ["mariiina"]
 
-var rows = $(".windowbg").add(".windowbg2").each(function(index, element) {
-  console.log("POST!")
-  var nickLink = $(element).find('a[href*="action=profile;u="]').first()[0]
-  var nick = nickLink.innerHTML.trim()
-  console.log("Author: " + nick)
+function ignorePosts() {
+    var rows = $(".windowbg").add(".windowbg2").each(function(index, element) {
+        var nickLink = $(element).find('a[href*="action=profile;u="]').first()[0]
+        var nick = nickLink.innerHTML.trim()
+        console.log("Author: " + nick)
 
-  if (ignored.indexOf(nick) >= 0) {
-    element.innerHTML = "<strong style='color: red; font-weight: bold'>IGNORED: " + nick + "</strong>";
-    //element.style.backgroundColor = "red"
-    //element.style.height = "17px"
-    //element.style.overflow = "hidden"
-    //element.style.display = "none"
-  }
-  //console.log(nickLink)
-  //console.log(element.innerHTML)      
-})
+        if (ignored.indexOf(nick) >= 0) {
+            element.innerHTML = "<strong style='color: red; font-weight: bold'>IGNORED: " + nick + "</strong>";
+        }
+    })
+}
+
+ignorePosts()
