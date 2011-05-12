1050afe14b0f134b371887b75bb1378f4abdd945
Hristo Deshev <hristo@deshev.com>
2011-05-12 19:39:24 +0300

    Added an options page to configure the users list.

--- ignore.js
+++ ignore.js
@@ -1,10 +1,7 @@
-var ignored = ["mariiina"]
-
-function ignorePosts() {
+function ignorePosts(ignored) {
     var rows = $(".windowbg").add(".windowbg2").each(function(index, element) {
         var nickLink = $(element).find('a[href*="action=profile;u="]').first()[0]
         var nick = nickLink.innerHTML.trim()
-        console.log("Author: " + nick)
 
         if (ignored.indexOf(nick) >= 0) {
             element.innerHTML = "<strong style='color: red; font-weight: bold'>IGNORED: " + nick + "</strong>";
@@ -12,4 +9,7 @@ function ignorePosts() {
     })
 }
 
-ignorePosts()
+chrome.extension.sendRequest({method: "getIgnored"}, function(response) {
+  console.log("Ignoring users: " + response.ignored)
+  ignorePosts(response.ignored)
+});
